6019d03ee6f9a0c4409d43cca89092c813143879
Ariel Angelo <116392268+Alfa9029@users.noreply.github.com>
2022-10-24 17:17:08 -0300

    Add files via upload

---
+++ TesteQ11.java
@@ -0,0 +1,20 @@
+package br.com.caelum.ed.vetores;
+
+import java.util.ArrayList;
+
+public class TesteQ11 {
+
+	public static void main(String[] args) {
+			
+		ArrayList vetorSemGenerics = new ArrayList();
+		ArrayList<Aluno> vetorComGenerics = new ArrayList<Aluno>();
+
+		Aluno aluno = new Aluno();
+		
+		vetorSemGenerics.add(aluno);
+		vetorComGenerics.add(aluno);
+
+		Aluno a1 = (Aluno) vetorSemGenerics.get(0);
+		Aluno a2 = vetorComGenerics.get(0);
+	}
+}
